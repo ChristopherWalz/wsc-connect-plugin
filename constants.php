ce6f3dc1c2b35a0fcfc6b3edb491852553483f04
mutec <webmaster@mysterycode.de>
2017-08-18 19:12:13 +0200

    auto-login to prevent a login frame before the notification is shown
    I'd recomment to use this only in combination with the setting to open the notifications using a WebView within the app. The parameters should be send with a POST-request if possible.
    Maybe POST will cause some unwanted "features"? Let's have a try.

---
+++ constants.php
@@ -0,0 +1,9 @@
+<?php
+
+define('MODULE_WSC_CONNECT', 1);
+define('WSC_CONNECT_PUSH_MODE', 'immediately');
+define('WSC_CONNECT_APP_ID', '000000aa000a0a0a0a00a000');
+define('WSC_CONNECT_APP_SECRET', 'aaaa0000-aa00-aa00-aa00-aaaaaa000000');
+define('WSC_CONNECT_INFO_ENABLE', 1);
+define('WSC_CONNECT_INFO_TIME', 30);
+define('WSC_CONNECT_SILENT_LOGIN', 1);

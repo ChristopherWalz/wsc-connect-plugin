875bd4afcf0aa23beca158175241d28f68e8dd9b
Christopher Walz <chris@cwalz.de>
2019-08-25 16:28:27 +0200

    Show info banner on iOS devices

--- files/js/CW.WSCConnect.js
+++ files/js/CW.WSCConnect.js
@@ -11,23 +11,33 @@ CW.WSCConnect = Class.extend({
 	init: function(cookiePrefix, cookieTime) {
 		this.cookieName = cookiePrefix + 'wscconnect';
 		this.cookieTime = parseInt(cookieTime);
+		this.links = {
+			android: 'https://play.google.com/store/apps/details?id=wscconnect.android&pcampaignid=wsc-plugin',
+			ios: 'https://apps.apple.com/us/app/wsc-connect/id1462270360'
+		};
+		this.text = {
+			android: WCF.Language.get('wcf.wsc_connect.info.android'),
+			ios: WCF.Language.get('wcf.wsc_connect.info.ios')
+		};
 
 		var userAgent = window.navigator.userAgent.toLowerCase();
-		if (jQuery.browser.android && 
+		if ((jQuery.browser.android || jQuery.browser.iOS) && 
 			userAgent.indexOf('wsc-connect mobile browser') === -1 &&
 			document.cookie.match(new RegExp('(^| )' + this.cookieName + '=([^;]+)')) === null) {
 
+			var browser = (jQuery.browser.android) ? 'android' : 'ios';
 			var wscConnectInfo = document.getElementById('wscConnectInfo');
 
-			wscConnectInfo.style.display = 'block';
+			$('.text', $(wscConnectInfo)).text(this.text[browser]);
+
+			wscConnectInfo.style.display = 'flex';
 
 			wscConnectInfo.addEventListener("click", function(e) {
 				e.preventDefault();
 				
 				this.setCookie();
 
-				var href = $(wscConnectInfo).data('href');
-				window.location = href;
+				window.location = this.link[browser];
 			}.bind(this));
 
 
